d28b5d8caae55de40b2b574f99ed699c5b73920d
Brijesh Shah <brijeshshah86@gmail.com>
2024-12-29 22:41:48 +0530

    Allow different delimeter

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,8 +2,20 @@ class Calculator
   def self.add(numbers)
     return 0 if numbers.nil? || numbers.empty?
 
-    delimiter = /,/
+    delimiter = get_delimiter(numbers)
     num_array = numbers.split(/#{delimiter}/).map(&:to_i)
     num_array.sum
+  end
+
+  private
+
+  def self.get_delimiter(numbers)
+    delimiter = /,/
+
+    if numbers.start_with?("//")
+      delimiter_info, _numbers = numbers.split("\n", 2)
+      delimiter = Regexp.escape(delimiter_info[2..-1])
+    end
+    delimiter
   end
 end
