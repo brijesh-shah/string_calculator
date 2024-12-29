752d9369c3adcd3a70ac044318f0b6edde5d2b15
Brijesh Shah <brijeshshah86@gmail.com>
2024-12-29 22:31:36 +0530

    Comma delimeter added for string calculator

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,5 +1,9 @@
 class Calculator
   def self.add(numbers)
     return 0 if numbers.nil? || numbers.empty?
+
+    delimiter = /,/
+    num_array = numbers.split(/#{delimiter}/).map(&:to_i)
+    num_array.sum
   end
 end
